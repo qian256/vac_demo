e2d4b97c1635c3c560ee8a0b89a37fd5947652c2
qian256 <joewalker.ql@gmail.com>
2016-10-07 10:08:38 -0400

    initial commit

---
+++ Assets/DOF.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class DOF : MonoBehaviour {
+    private Text text;
+    public DepthTuner dt; 
+
+    void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    void LateUpdate() {
+        text.text = "DOF: " + dt.getCurrentDOF();
+    }
+}
---
+++ Assets/DepthTuner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityStandardAssets.ImageEffects;
+
+public class DepthTuner : MonoBehaviour {
+
+    private CardboardReticle rectile;
+    
+    public float maxFocalDistance = 10.0f;
+    public float minFocalDistance = 0.0f;
+    private float currentFocalDistance = 0.1f;
+    private float tempFocalDistance = 0.0f;
+
+    public DepthOfField leftCamera, rightCamera;
+    
+    public float getCurrentDOF() {
+        return currentFocalDistance;
+    }
+    
+    
+    // Use this for initialization
+    void Start () {
+        rectile = GetComponent<CardboardReticle>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Cardboard.SDK.BackButtonPressed) {
+            Application.Quit();
+        }
+        if ( rectile.onUI) {
+            leftCamera.aperture = 0;
+            rightCamera.aperture = 0;
+        }
+        else {
+            leftCamera.aperture = 0.4f;
+            rightCamera.aperture = 0.4f;
+        }
+        tempFocalDistance = rectile.currentDistance;
+        currentFocalDistance = Mathf.Clamp(tempFocalDistance, minFocalDistance, maxFocalDistance);
+        leftCamera.focalLength = currentFocalDistance;
+        rightCamera.focalLength = currentFocalDistance;
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+    
+}
